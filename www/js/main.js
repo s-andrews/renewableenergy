cc223c6144d19a7615c181b11aa543b7a61ff7fc
Simon Andrews <simon@proteo.me.uk>
2025-03-09 16:53:34 +0000

    Added main clickable links

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,95 @@
-$( document ).ready(function() {
+var solar = false
+var turbine = false
+var battery = false
+var battery_level = 0
+var wind = true
+var weather = "sun"
 
+$( document ).ready(function() {
+    $(".clickable").click(something_clicked)
+
+    redraw()
 })
+
+function something_clicked() {
+    let name = $(this).attr('id')
+
+    console.log("Name is "+name)
+
+    if (name == "solar") {
+        solar = !solar
+    }
+
+    if (name == "battery") {
+        battery = !battery
+    }
+
+    if (name == "turbine") {
+        turbine = !turbine
+    }
+
+    if (name == "wind") {
+        wind = !wind
+    }
+
+    if (name == "weather") {
+        if (weather == "sun") {
+            weather = "grey_cloud"
+        }
+        else if (weather == "grey_cloud") {
+            weather = "black_cloud"
+        }
+        else {
+            weather = "sun"
+        }
+    }
+
+
+    // Sanity check
+
+    // Can't have a battery without either solar or turbine
+    if (battery & (!solar & !turbine)) {
+        battery=false
+    }
+
+
+    redraw()
+
+}
+
+function redraw() {
+
+    if (solar) {
+        $("#solar").css("opacity",1)
+    }
+    else {
+        $("#solar").css("opacity",0.1)
+    }
+
+    if (turbine) {
+        $("#turbine").css("opacity",1)
+    }
+    else {
+        $("#turbine").css("opacity",0.1)
+    }
+
+    if (battery) {
+        $("#battery").css("opacity",1)
+    }
+    else {
+        $("#battery").css("opacity",0.1)
+    }
+
+    if (wind) {
+        $("#wind").css("opacity",1)
+    }
+    else {
+        $("#wind").css("opacity",0.1)
+    }
+
+    $("#weather").attr("src","images/"+weather+".svg")
+
+
+
+}
+
