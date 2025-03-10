e4a5774dcdfadd6669ba17719a46c707a83e5cef
Simon Andrews <simon@proteo.me.uk>
2025-03-10 21:32:13 +0000

    Added spark animations to solar and turbine

--- www/js/main.js
+++ www/js/main.js
@@ -1,15 +1,39 @@
-var solar = false
+var solar = true
 var turbine = false
 var battery = false
 var battery_level = 0
 var wind = true
 var weather = "sun"
+var spark = new Image()
+spark.src = "images/spark.svg"
+
+var renewable_line_percent = 100
+var pylon_line_percent = 0
+var battery_line_percent = 0
 
 $( document ).ready(function() {
     $(".clickable").click(something_clicked)
 
+    $(window).on("resize",redraw_lines)
+
     redraw()
+
+    setInterval(update_spark_positions,200)
+
 })
+
+function update_spark_positions () {
+    if ((solar & (weather == "sun" | weather == "grey_cloud")) | (turbine & wind)) {
+        renewable_line_percent += 5
+        if (renewable_line_percent > 100) {
+            renewable_line_percent -= 100
+        }
+    }
+
+
+    redraw_lines()
+}
+
 
 function something_clicked() {
     let name = $(this).attr('id')
@@ -92,6 +116,11 @@ function redraw() {
     $("#weather").attr("src","images/"+weather+".svg")
 
 
+    redraw_lines()
+}
+
+function redraw_lines() {
+
     // Draw canvas lines
     let canvas = $("#maincanvas")[0].getContext("2d")
 
@@ -114,11 +143,31 @@ function redraw() {
         canvas.lineTo(x3,y3)
         if (weather == "sun" | weather == "grey_cloud") {
             canvas.strokeStyle = "green"
+            canvas.stroke()
+
+            // We also need to draw a spark on the line
+            let total_length = (x2-x1)+(y3-y2)
+            let distance_on_line = Math.floor((total_length/100)*renewable_line_percent)
+
+            let solar_x = 0
+            let solar_y = 0
+            if (distance_on_line < (x2-x1)) {
+                solar_x = x1+distance_on_line
+                solar_y = y1
+            }
+            else {
+                solar_x = x2
+                solar_y = y2+(distance_on_line - (x2-x1))
+            }
+            solar_x -= spark.width/2
+            solar_y -= spark.height/2
+            canvas.drawImage(spark,solar_x,solar_y)
+
         }
         else {
             canvas.strokeStyle = "gray"
+            canvas.stroke()
         }
-        canvas.stroke()
     }
 
     if (turbine) {
@@ -135,11 +184,30 @@ function redraw() {
         canvas.lineTo(x3,y3)
         if (wind) {
             canvas.strokeStyle = "green"
+            canvas.stroke()
+
+            // We also need to draw a spark on the line
+            let total_length = (x1-x2)+(y3-y2)
+            let distance_on_line = Math.floor((total_length/100)*renewable_line_percent)
+
+            let turbine_x = 0
+            let turbine_y = 0
+            if (distance_on_line < (x1-x2)) {
+                turbine_x = x1-distance_on_line
+                turbine_y = y1
+            }
+            else {
+                turbine_x = x2
+                turbine_y = y1+(distance_on_line - (x1-x2))
+            }
+            turbine_x -= spark.width/2
+            turbine_y -= spark.height/2
+            canvas.drawImage(spark,turbine_x,turbine_y)
         }
         else {
             canvas.strokeStyle = "gray"
+            canvas.stroke()
         }
-        canvas.stroke()
 
     }
 
